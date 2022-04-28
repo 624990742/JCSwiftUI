4fa783f65343ea4aeb9a059f541ce786cfe6fff3
jiachen <624990742@qq.com>
2022-04-28 15:18:55 +0800

    初始化工程

---
+++ SwiftUIDemo/SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by jcmac on 2022/4/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
