bd35c13c285598d91fca822ae917ff222f022c15
jiachen <624990742@qq.com>
2022-05-11 10:53:23 +0800

    添加绘制图标代码

---
+++ SwiftUIDemo/SwiftUIDemo/SmallExampleDemo/ LandmarksAppDemo/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,23 @@
+//
+//  RotatedBadgeSymbol.swift
+//  SwiftUIDemo
+//
+//  Created by jcmac on 2022/5/10.
+//
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
