59b27ad37f2c2c0176322fb55c34de4a0c0cd4ab
jiachen <624990742@qq.com>
2022-06-22 16:22:00 +0800

    添加与UIkit框架交互的逻辑。

---
+++ SwiftUIDemo/SwiftUIDemo/SmallExampleDemo/ LandmarksAppDemo/Views/PageView/PageView.swift
@@ -0,0 +1,30 @@
+//
+//  pageView.swift
+//  SwiftUIDemo
+//
+//  Created by jcmac on 2022/6/22.
+//
+
+import SwiftUI
+import CoreGraphics
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages,currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
+}
+
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
